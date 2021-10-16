9946680dc467a01db94b5288a3650a2e11d9aa22
jiayenli <x9276550@gmail.com>
2021-10-16 13:34:02 +0800

    安裝bootstrap(版本5.1.3)

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,14 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
+
+<script>
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+export default {
+  name: 'App'
+  
+}
+</script>
