c327f3aea39fc617fdf5742ddde52758a674c345
jiayenli <x9276550@gmail.com>
2021-10-18 23:38:51 +0800

    新增首頁路由

--- src/router/index.js
+++ src/router/index.js
@@ -13,11 +13,14 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import( '../views/About.vue')
+  },
+  {
+    path: '/goFarmmy',
+    name: 'About',
+    component: () => import('../views/HomePage.vue')
+  },
+
 ]
 
 const router = new VueRouter({
