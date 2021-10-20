70a58724161610ed6d0cd0d129453fa663e87b2f
jiayenli <x9276550@gmail.com>
2021-10-21 00:48:11 +0800

    完成商品頁面版面初稿切版(不含js功能)

---
+++ src/components/ProductList.vue
@@ -0,0 +1,156 @@
+<template>
+<div class="product-items">
+  <div class="product-content-items-card" v-for="item in initialItems" :key="item.id">
+    <div class="product-content-items-card-img">
+      <img :src="item.image" />
+      <h4>
+        {{ item.description | descriptionLength }}
+      </h4>
+    </div>
+    <h2>{{ item.name }} | {{ item.origin }}</h2>
+    <div class="product-content-items-card-specification">
+      <i class="fas fa-tag"></i> {{ item.specification }}
+    </div>
+
+    <div class="product-content-items-card-price">
+      價格：{{ item.price }}元/箱
+    </div>
+    <div class="product-content-items-card-panel">
+      <div class="product-content-items-card-panel-number">
+        <span class="product-content-items-card-panel-number-icon">+</span>
+        <span class="product-content-items-card-panel-number-icon">-</span>
+        <input
+          ref="getValue"
+          value="1"
+          class="product-content-items-card-panel-number-input"
+          id="number"
+          type="number"
+          required
+        />
+        <span>個</span>
+      </div>
+      <div class="product-content-items-card-panel-button">加入購物車</div>
+    </div>
+  </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/scss/color.scss";
+.product-items {
+        display: flex;
+      flex-wrap: wrap;
+      border-top: solid $color-brown 4px;
+      padding-top:5%;
+}
+.product-content-items-card {
+  margin-right: 4%;
+  height: 450px;
+  width: 300px;
+  h2 {
+    margin-top: 3%;
+    text-align: center;
+    color: $color-brown;
+  }
+  &-specification,
+  &-price {
+    text-align: center;
+    color: $color-brown;
+    font-size: 14px;
+  }
+  &-img {
+    position: relative;
+    display: flex;
+    height: 60%;
+    img {
+      object-fit: cover;
+      transition: 0.5s;
+    }
+    h4 {
+      position: absolute;
+      color: $color-brown;
+      top: 0;
+      right: 50%;
+      transform: translateX(50%);
+      width: 90%;
+      opacity: 0;
+      word-wrap: break-word;
+      line-height: 200%;
+      transition: 0.5s;
+    }
+    &:hover {
+      img {
+        opacity: 0.3;
+      }
+      h4 {
+        opacity: 1;
+      }
+    }
+  }
+
+  &-panel {
+    margin-top: 5%;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    &-number {
+      display: flex;
+      &-icon {
+        display: flex;
+        height: 25px;
+        width: 25px;
+        margin-right: 3%;
+        align-items: center;
+        justify-content: center;
+        font-weight: bolder;
+        background-color: #edda7e;
+        transition: 0.3s;
+        color: $color-brown;
+        &:hover {
+          background-color: $color-brown;
+          color: white;
+        }
+      }
+
+      input {
+        display: inline-block;
+        width: 20%;
+        border: 1px $color-brown solid;
+        margin: 0 2%;
+        height: 25px;
+        padding-left: 2%;
+      }
+    }
+    &-button {
+      background-color: $color-brown;
+      text-align: center;
+      color: white;
+      white-space: nowrap;
+      font-size: 14px;
+      padding: 2%;
+      &:hover {
+        animation: logoFocus 0.1s linear infinite;
+        cursor: pointer;
+      }
+    }
+  }
+}
+</style>
+
+<script>
+import { descriptionLengthFilter} from "./../utils/mixins";
+export default {
+  props: {
+    initialItems: {
+      require: true
+    }
+  },
+   mixins:[descriptionLengthFilter],
+  data() {
+    return{
+      items : this.initialItems
+    }
+  }
+  
+}
+</script>
---
+++ src/views/Product.vue
@@ -0,0 +1,203 @@
+<template>
+  <div class="product">
+    <Navbar />
+    <CartNavbar />
+    <div class="product-cover">
+      <img class="product-cover-title" src="./../assets/product-icon-1.png" />
+      <img class="product-cover-img" src="./../assets/product-img-1.png" />
+    </div>
+    <div class="product-content">
+      <div class="product-content-nav">
+        <div class="product-content-nav-search">
+          <div>
+            <i class="fas fa-search"></i>
+          </div>
+          <input placeholder="火龍果！" type="text" required />
+        </div>
+        <div class="product-content-nav-category">
+          <h2>商品分類</h2>
+          <div class="product-content-nav-category-name">
+          <div >
+            <h3>全部</h3>
+          </div>
+          <div >
+            <h3>果物</h3>
+          </div>
+          <div >
+            <h3>蔬菜</h3>
+          </div>
+          </div>
+        </div>
+      </div>
+      <div class="product-content-items">
+        <h2>商品一覽</h2>
+        <!--商品卡片區-->
+        <ProductList :initialItems="items" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+import CartNavbar from "@/components/CartNavbar.vue";
+import ProductList from "@/components/ProductList.vue";
+import ProductAPI from "./../apis/products";
+import Swal from "sweetalert2";
+
+export default {
+  name: "Home",
+  components: {
+    Navbar,
+    CartNavbar,
+    ProductList,
+  },
+
+  data() {
+    return {
+      items: [],
+    };
+  },
+
+  methods: {
+    async fetchData() {
+      try {
+        const response = await ProductAPI.getProducts({ categoryId: "" });
+        this.items = {
+          ...response.data,
+        };
+      } catch (error) {
+        Swal.fire({
+          icon: "error",
+          title: "商品載入錯誤，請稍後再試",
+          toast: true,
+          showConfirmButton: false,
+          timer: 3000,
+        });
+      }
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/color.scss";
+
+.product {
+  margin: 0 0;
+  //outline: 1px red solid;
+  width: 100vw;
+  height: 100vh;
+  overflow: scroll;
+  background-image: url("./../assets/home-background.png");
+  &-cover {
+    position: relative;
+    &-title {
+      position: absolute;
+      max-height: 90px;
+      max-width: 250px;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      top: 50%;
+    }
+    &-img {
+      margin-top: 1%;
+      height: 300px;
+      width: 100%;
+      object-fit: cover;
+    }
+  }
+  &-content {
+    display: flex;
+    margin-top: 3%;
+    &-nav {
+      width: 30%;
+      padding-left: 10%;
+      padding-right: 3%;
+      // outline: green 1px solid;
+      &-search {
+        display: flex;
+        //padding: 2%;
+        //background-color: $color-brown;
+        input {
+          margin-left: 5%;
+          height: 40px;
+          border: 4px $color-brown solid;
+          padding-left: 2%;
+          width: 100%;
+        }
+        div {
+          flex-shrink: 0;
+          display: flex;
+          height: 40px;
+          width: 40px;
+          background-color: $color-brown;
+          justify-content: center;
+          align-items: center;
+          color: white;
+        }
+      }
+      &-category {
+        margin-top: 18%;
+        border: 4px $color-brown solid;
+        background-color: $color-brown;
+
+        h2 {
+          padding: 2%;
+          color: white;
+        }
+        &-name {
+          background-image: url("./../assets/home-background.png");
+          padding: 2%;
+          padding-top: 5%;
+          h3 {
+            margin-bottom:5% ;
+          }
+          h3::before {
+            content: "";
+            display: inline-block;
+            height: 10px;
+            width: 10px;
+            border-radius: 50%;
+            border: 4px $color-brown solid;
+            margin-right: 5%;
+            transition: 0.3s;
+          }
+          h3:hover {
+            &::before {
+              background-color: $color-brown;
+            }
+          }
+        }
+      }
+    }
+    &-items {
+      padding-right: 10%;
+      width: 70%;
+
+      //outline: red 1px solid;
+    }
+  }
+}
+
+@keyframes logoFocus {
+  0% {
+    transform: rotate(0deg);
+  }
+  25% {
+    transform: rotate(-2deg);
+  }
+  50% {
+    transform: rotate(0deg);
+  }
+  75% {
+    transform: rotate(2deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+}
+</style>
